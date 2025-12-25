b01fb49f9cdfd7762599a8a40f97b5bb348c8719
bhoomirajpoot <bhoomirajpoot2005@gmail.com>
2025-12-25 14:37:30 +0530

    nodejs

---
+++ React/Demo/src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function App() {
+  let a = 5;
+  let b = 10;
+
+  console.log(a + b);
+
+  return (
+    // emptytag
+    <>
+      <h1>Hello World</h1>
+      <div>
+        this is my first React App
+      </div>
+
+    </>
+  );
+}
+
+export default App;
