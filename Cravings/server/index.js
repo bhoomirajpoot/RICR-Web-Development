456421ac1614786ff9276c732ccc2644acfa2c64
bhoomirajpoot <bhoomirajpoot2005@gmail.com>
2026-01-11 18:39:58 +0530

    PROJECT

---
+++ Cravings/server/index.js
@@ -0,0 +1,22 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import express from 'express';
+import connectDB from './src/config/db.js';
+
+const app = express();
+
+app.use(express.json());
+
+// app.use("/auth", Authrouter);
+
+app.get("/", (req, res) => {
+  console.log("server is working");
+
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log("server started at Port:", port);
+  connectDB();
+});
