f3752e020a2298c7640d50bcd46ddbf692853b55
bhoomirajpoot <bhoomirajpoot2005@gmail.com>
2026-01-11 19:18:09 +0530

    PROJECT

--- Cravings/server/index.js
+++ Cravings/server/index.js
@@ -2,10 +2,12 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import express from 'express';
+import cors from 'cors';
 import connectDB from './src/config/db.js';
 
 const app = express();
 
+app.use(cors({origin:"http://localhost:5173"}));
 app.use(express.json());
 
 // app.use("/auth", Authrouter);
