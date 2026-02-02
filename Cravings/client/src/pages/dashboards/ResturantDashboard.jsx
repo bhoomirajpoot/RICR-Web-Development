34ebb0565e9b93b7c78a5f1469bfe2d3b6f12700
bhoomirajpoot <bhoomirajpoot2005@gmail.com>
2026-02-02 19:52:01 +0530

    client

--- Cravings/client/src/pages/dashboards/ResturantDashboard.jsx
+++ Cravings/client/src/pages/dashboards/ResturantDashboard.jsx
@@ -1,17 +1,17 @@
 import React, { useState, useEffect } from "react";
 import RestaurantSideBar from "../../components/restaurantDashboard/RestaurantSideBar";
 import RestaurantOverview from "../../components/restaurantDashboard/RestaurantOverview";
-import RestaurantOrders from "../../components/restaurantDashboard/RestaurantOrders";
-import RestaurantMenu from "../../components/restaurantDashboard/RestaurantHelpDesk";
 import RestaurantProfile from "../../components/restaurantDashboard/RestaurantProfile";
-import RestaurantHelpDesk from "../../components/restaurantDashboard/RestaurantEarnings";
+import RestaurantMenu from "../../components/restaurantDashboard/RestaurantMenu";
+import RestaurantOrders from "../../components/restaurantDashboard/RestaurantOrders";
+import RestaurantEarnings from "../../components/restaurantDashboard/RestaurantEarnings";
+import RestaurantHelpDesk from "../../components/restaurantDashboard/RestaurantHelpDesk";
 import { useAuth } from "../../context/AuthContext";
 import { useNavigate } from "react-router-dom";
 
-const ResturantDashboard = () => {
+const RestaurantDashboard = () => {
   const { role, isLogin } = useAuth();
   const navigate = useNavigate();
-
   const [active, setActive] = useState("overview");
   const [isCollapsed, setIsCollapsed] = useState(false);
 
@@ -19,50 +19,49 @@ const ResturantDashboard = () => {
     if (!isLogin) {
       navigate("/login");
     }
-  }, [isLogin, navigate]);
+  });
 
   if (role !== "manager") {
     return (
-      <div className="p-3">
-        <div className="border rounded shadow p-5 w-4xl mx-auto text-center bg-gray-100">
-          <div className="text-5xl text-red-600">⊗</div>
-          <div className="text-xl">
-            You are not logged in as Restaurant Manager. Please login again.
+      <>
+        <div className="p-3">
+          <div className="border rounded shadow p-5 w-4xl mx-auto text-center bg-gray-100">
+            <div className="text-5xl text-red-600">⊗</div>
+            <div className="text-xl">
+              You are not logged in as Restaurant Manager. Please login again.
+            </div>
           </div>
         </div>
-      </div>
+      </>
     );
   }
 
   return (
-    <div className="w-full h-[90vh] flex">
-   
-      <div
-        className={`bg-(--color-background) duration-300 ${
-          isCollapsed ? "w-2/60" : "w-12/60"
-        }`}
-      >
-        <RestaurantSideBar
-          active={active}
-          setActive={setActive}
-          isCollapsed={isCollapsed}
-          setIsCollapsed={setIsCollapsed}
-        />
+    <>
+      <div className="w-full h-[90vh] flex">
+        <div
+          className={`bg-(--color-background) duration-300 ${
+            isCollapsed ? "w-2/60" : "w-12/60"
+          }`}
+        >
+          <RestaurantSideBar
+            active={active}
+            setActive={setActive}
+            isCollapsed={isCollapsed}
+            setIsCollapsed={setIsCollapsed}
+          />
+        </div>
+        <div className={`${isCollapsed ? "w-58/60" : "w-48/60"} duration-300`}>
+          {active === "overview" && <RestaurantOverview />}
+          {active === "profile" && <RestaurantProfile />}
+          {active === "menu" && <RestaurantMenu />}
+          {active === "orders" && <RestaurantOrders />}
+          {active === "earnings" && <RestaurantEarnings />}
+          {active === "helpdesk" && <RestaurantHelpDesk />}
+        </div>
       </div>
-
-      <div
-        className={`${
-          isCollapsed ? "w-58/60" : "w-48/60"
-        } duration-300`}
-      >
-        {active === "overview" && <RestaurantOverview />}
-        {active === "orders" && <RestaurantOrders />}
-        {active === "menu" && <RestaurantMenu />}
-        {active === "profile" && <RestaurantProfile />}
-        {active === "helpdesk" && <RestaurantHelpDesk />}
-      </div>
-    </div>
+    </>
   );
 };
 
-export default ResturantDashboard;
+export default RestaurantDashboard;
