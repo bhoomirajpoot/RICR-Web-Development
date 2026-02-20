c02e3f4082150035f1c3c93996d749b7b96da97d
bhoomirajpoot <bhoomirajpoot2005@gmail.com>
2026-02-20 23:54:01 +0530

    commit

--- MobileQuiz/client/src/pages/Join.jsx
+++ MobileQuiz/client/src/pages/Join.jsx
@@ -1,33 +1,35 @@
 import { useState } from "react";
-import socket from "../socket";
+import Layout from "../components/Layout";
 
 function Join() {
   const [name, setName] = useState("");
 
   const handleJoin = () => {
     if (!name) return;
-    socket.emit("join", name);
     alert("Joined as " + name);
   };
 
   return (
-    <div style={{ textAlign: "center", marginTop: "100px" }}>
-      <h2>Join Quiz</h2>
+    <Layout>
+      <h2 className="text-2xl font-bold text-white mb-6">
+        Join Quiz
+      </h2>
 
       <input
         type="text"
         placeholder="Enter your name"
+        className="w-full p-3 rounded-lg mb-4 outline-none"
         value={name}
         onChange={(e) => setName(e.target.value)}
-        style={{ padding: "8px" }}
       />
 
-      <br /><br />
-
-      <button onClick={handleJoin} style={{ padding: "10px 20px" }}>
-        Join
+      <button
+        onClick={handleJoin}
+        className="w-full py-3 bg-yellow-400 rounded-lg font-semibold hover:scale-105 transition"
+      >
+        Join Now
       </button>
-    </div>
+    </Layout>
   );
 }
 
