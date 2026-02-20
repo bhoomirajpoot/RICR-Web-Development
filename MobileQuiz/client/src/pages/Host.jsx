3416f0c9531d05699890ebd4e744a8255fd7f94e
bhoomirajpoot <bhoomirajpoot2005@gmail.com>
2026-02-20 23:44:08 +0530

    commit

---
+++ MobileQuiz/client/src/pages/Host.jsx
@@ -0,0 +1,10 @@
+function Host() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h2>Host Panel</h2>
+      <p>Quiz host controls will come here</p>
+    </div>
+  );
+}
+
+export default Host;
