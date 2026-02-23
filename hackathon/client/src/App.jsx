2fe323e256d6cb37b7d4c82d961d164c02d9b187
bhoomirajpoot <bhoomirajpoot2005@gmail.com>
2026-02-23 17:12:17 +0530

    commit

---
+++ hackathon/client/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-gradient-to-r from-indigo-600 to-purple-600 h-screen flex items-center justify-center text-white text-4xl font-bold">
+      Premium UI Activated 🚀
+    </div>
+  );
+}
+
+export default App;
---
+++ hackathon/client/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <div>
+      <h1>AI Study Planner</h1>
+      <a href="/login">Login</a><br/>
+      <a href="/register">Register</a>
+    </div>
+  );
+}
+
+export default Home;
